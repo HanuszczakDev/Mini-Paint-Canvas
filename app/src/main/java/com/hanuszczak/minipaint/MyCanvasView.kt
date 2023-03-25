efd4195df102a5d6253deb0f61ae2e7a62f916f1
ahanuszczak <hanyhusajn@gmail.com>
2023-03-25 18:24:42 +0100

    implement touchStart()

--- app/src/main/java/com/hanuszczak/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/hanuszczak/minipaint/MyCanvasView.kt
@@ -22,6 +22,9 @@ class MyCanvasView(context: Context) : View(context) {
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
 
+    private var currentX = 0f
+    private var currentY = 0f
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
@@ -47,7 +50,12 @@ class MyCanvasView(context: Context) : View(context) {
         return true
     }
 
-    private fun touchStart() {}
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
 
     private fun touchMove() {}
 
