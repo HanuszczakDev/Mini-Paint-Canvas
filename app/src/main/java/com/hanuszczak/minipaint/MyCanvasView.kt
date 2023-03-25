04f239e0c02ab6e1577cd50583fb5710945c2c4f
ahanuszczak <hanyhusajn@gmail.com>
2023-03-25 18:18:21 +0100

    Setup Pain for Drawing | Path Object

--- app/src/main/java/com/hanuszczak/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/hanuszczak/minipaint/MyCanvasView.kt
@@ -3,14 +3,20 @@ package com.hanuszczak.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f // has to be float
 
 class MyCanvasView(context: Context) : View(context) {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -24,4 +30,19 @@ class MyCanvasView(context: Context) : View(context) {
         super.onDraw(canvas)
         canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
     }
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+    private var path = Path()
 }
